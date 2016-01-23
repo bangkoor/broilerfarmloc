2c6937826d1dfa7d02ae92414502cd0cd3191d5d
bangkoor <rifishere@gmail.com>
2016-01-23 14:23:23 +0700

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ $db_host="localhost";
 $db_port="5433";
 $db_name="broiler_farmloc";
 $db_user="postgres";
-$db_password="p4tri0t45";
+$db_password="pass";
 $pg_conn_string="host=".$db_host." port=".$db_port." dbname=".$db_name." user=".$db_user." password=".$db_password;
 
 if(!$pg_conn_string){
@@ -13,10 +13,4 @@ if(!$pg_conn_string){
 } ELSE {
 	$warningDB = NULL;
 }
-
-//path bin Postgis
-$PostGIS_BIN="C:/Program Files/PostgreSQL/9.1/bin";
-
-
-
 ?>
